2f28c93a3664fedb1447317db4052b869855f5a8
Bistrel2002 <bistrel.tsangue@algosup.com>
2024-11-06 13:43:14 +0100

    skills
    professional skills

--- src/main.js
+++ src/main.js
@@ -24,3 +24,22 @@ menuIcon.onclick = () =>{
     navbar.classList.toggle('active')
 }
 
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots * marked / 100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+
+    elem.innerHTML = points;
+
+    const pointMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i < percent; i++) {
+        pointMarked[i].classList.add('marked');
+    }
+});
